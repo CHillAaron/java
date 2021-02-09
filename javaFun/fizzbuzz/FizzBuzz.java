06e50c9c2dcac1fe2a141b9f26d0581a34dae490
Aaron <chillaaron21@gmail.com>
2021-02-08 20:52:09 -0500

    FizzBuzz

---
+++ javaFun/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,16 @@
+public class FizzBuzz {
+    public String buzzFizz(int number) {
+        // fizzbuzz logic here
+        if(number%3 == 0 && number%5 ==0){
+            System.out.println("FizzBuzz");
+            return "FizzBuzz";
+        }else if(number%5 == 0 ) {
+            System.out.println("Buzz");
+            return "Buzz";
+        }else if (number%3 == 0){
+            System.out.println("Fizz");
+            return "Fizz";
+        }else System.out.println("Try another number");
+        return "Try another number";
+    }
+}
---
+++ javaFun/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,10 @@
+public class FizzBuzzTest{
+    public static void main(String[] args) {
+        FizzBuzz fizzy = new FizzBuzz();
+        fizzy.buzzFizz(15);
+        fizzy.buzzFizz(5);
+        fizzy.buzzFizz(3);
+        fizzy.buzzFizz(2);
+
+    }
+}
